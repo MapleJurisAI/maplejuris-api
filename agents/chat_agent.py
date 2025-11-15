838922ec185eb43813da10057d03e771e2dc7004
Abdu Radi <92768651+AbduKhRadi@users.noreply.github.com>
2025-11-15 00:37:09 -0500

    Issue #4: Setting up the chat agent sleleton

---
+++ agents/chat_agent.py
@@ -0,0 +1,19 @@
+"""
+the class will define and agent that can process a user question and return a response
+an intermediat step will be adding a shwo prompt function for debugging
+"""
+
+
+class ChatAgent:
+    def __init__(self):
+        pass
+
+    def show_prompt(self, question: str):
+        pass
+
+    def process(self, question: str):
+        pass
+
+
+if __name__ == "__main__":
+    print("Hello world!")
