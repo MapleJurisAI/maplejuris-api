d9e84685d4fef53c9f1433ae178aaa0e8ed48ba8
Abdu Radi <92768651+AbduKhRadi@users.noreply.github.com>
2025-11-23 19:09:17 -0500

    Greated the graph class structure
    Since it will be one node active node class and depends on a router to handle the error

---
+++ graphs/chat_graph.py
@@ -0,0 +1,22 @@
+class ChatGraph:
+    def __init__(self):
+        pass
+
+    def _build_graph(self):
+        pass
+
+    def _handle_error(self):
+        pass
+
+    def node(self):
+        pass
+
+    def _node_router(self):
+        pass
+
+    def run(self, question: str):
+        pass
+
+
+if __name__ == "__main__":
+    print("Graph established")
